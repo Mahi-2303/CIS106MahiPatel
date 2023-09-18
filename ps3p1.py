30db47141f09ee001452629d0ea0dd5686e67315
Mahi-2303 <144925406+Mahi-2303@users.noreply.github.com>
2023-09-18 09:43:08 -0500

    Add files via upload

---
+++ ps3p1.py
@@ -0,0 +1,8 @@
+e1=int(input("Enter exam 1 score : "))
+x=e1*0.60
+print("Total score of firrst exam is : ",x)
+e2=int(input("Enter exam 2 score : "))
+y=e2*0.40
+print("Total score of second exam is : ",y)
+f1=x+y
+print("Your final score is : ",f1)
